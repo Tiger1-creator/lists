afea13096c23aa8a57fec10f55a555a743717e93
Tiger1-creator <olivierniyongabo200@gmail.com>
2025-05-04 00:11:20 +0200

    new

---
+++ pthon/cal.py
@@ -0,0 +1,5 @@
+def add():
+    input1 = int(input("Enter first number: "))
+    input2 = int(input("Enter second number: "))
+    result = input1 + input2
+    print(f"The sum of {input1} and {input2} is {result}.")
